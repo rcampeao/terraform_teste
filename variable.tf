01386c69a836a12429c6dacc071dff570fcbab32
Vanessa Helena Souza Fernandes <vanessa.souza@linx.com.br>
2018-10-12 19:41:03 -0300

    second

---
+++ variable.tf
@@ -0,0 +1,15 @@
+#variable "" {}
+
+variable "access_key" {
+	description		= ""
+}
+
+variable "secret_key" {
+	description		= ""
+
+}
+
+variable "region" {
+	description		= ""
+	default	= ["us-east-1"]
+}
